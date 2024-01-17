6f12ac3c21daf059b585cee460e40c88185602d6
manoj-101-dev <manoj19545@gmail.com>
2024-01-17 10:21:33 +0530

    minor chnages

--- controllers/leaveController.js
+++ controllers/leaveController.js
@@ -6,33 +6,6 @@ import { ObjectId } from "mongodb";
 
 dotenv.config();
 
-// Email configuration using nodemailer (Gmail as an example)
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
-
-// Function to send an email
-const sendEmail = (userEmail, application) => {
-  const mailOptions = {
-    from: userEmail, // Sender email
-    to: process.env.EMAIL, // Recipient's email
-    subject: "New Leave Application Submitted",
-    text: `Dear User,\n\nYour leave application details are as follows:\n\nDays: ${application.days}\nFrom: ${application.from}\nTo: ${application.to}\nReason: ${application.options}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error occurred while sending email:", error);
-    } else {
-      console.log("Email sent:", info.response);
-    }
-  });
-};
-
 // Endpoint to handle Leave Applications form submission
 export const leaveApplication = async (req, res) => {
   const newApplication = req.body;
@@ -106,4 +79,31 @@ export const getAllLeaveApplications = async (req, res) => {
       .status(500)
       .json({ message: "Error retrieving user's leave applications" });
   }
+};
+
+// Email configuration using nodemailer (Gmail as an example)
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+// Function to send an email
+const sendEmail = (userEmail, application) => {
+  const mailOptions = {
+    from: process.env.EMAIL, // Sender email
+    to: userEmail, // Recipient's email
+    subject: "New Leave Application Submitted",
+    text: `Dear User,\n\nYour leave application details are as follows:\n\nDays: ${application.days}\nFrom: ${application.from}\nTo: ${application.to}\nReason: ${application.options}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error occurred while sending email:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
 };
--- controllers/queriesController.js
+++ controllers/queriesController.js
@@ -81,10 +81,10 @@ const sendEmail = async (userEmail, newQuery) => {
     });
 
     const mailOptions = {
-      from: userEmail,
-      to: process.env.EMAIL,
+      from: process.env.EMAIL,
+      to: userEmail,
       subject: "Query Created",
-      text: `Your query "${newQuery.title}" has been created successfully.\n Our team will contact you soon and resolve your doubts ASAP`,
+      text: `Your query "${newQuery.title}" has been created successfully.\nOur team will contact you soon and resolve your doubts ASAP.`,
     };
 
     const info = await transporter.sendMail(mailOptions);
