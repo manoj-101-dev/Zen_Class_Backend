499ef8fe830b70ae1ecf253e0dc8549b2cba3eab
manoj-101-dev <manoj19545@gmail.com>
2024-01-17 11:05:48 +0530

    minor changes

--- auth/jwt.js
+++ auth/jwt.js
@@ -5,7 +5,7 @@ dotenv.config();
 const secretKey = process.env.SECRET_KEY;
 
 export function generateToken(userId) {
-  return jwt.sign({ userId }, secretKey, { expiresIn: "1h" });
+  return jwt.sign({ userId }, secretKey); // Token includes only userId and expires in 1 hour
 }
 
 export function verifyToken(token) {
@@ -16,21 +16,11 @@ export function verifyToken(token) {
     return null;
   }
 }
-
 export function authenticateToken(req, res, next) {
   const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: Token is missing" });
   }
-
-  jwt.verify(token, secretKey, (err, user) => {
-    if (err) {
-      console.error("JWT verification error:", err);
-      return res.status(403).json({ message: "Forbidden: Invalid token" });
-    }
-
-    req.user = user;
-    next();
-  });
+  next();
 }
